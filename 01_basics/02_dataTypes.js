a2e475f394d6393e3e6fa911839d18a9167c7454
Mrinalini Bhattacharjee <138326214+mrinalini17@users.noreply.github.com>
2024-12-12 21:19:55 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+//code should always be readable, take care about spaces and ;
+
+let name="Mrinalini"
+let age=21
+let isAdult=true
+
+//number(2^53)
+//bigint
+//string("")
+//boolean
+//null(standalone value)=>can be assigned to a variable, empty
+//undefined=> value not assigned
+//symbol(uniqueness)
+
+
+//object
+
+console.log(typeof null)
+//null is an object whereas undefined is a type
