4e6e85e9b3b4c94a24b24e32811ed3f6b01ad8da
Mrinalini Bhattacharjee <138326214+mrinalini17@users.noreply.github.com>
2024-12-12 20:46:37 +0000

    Learnt Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=144553 //can't be changed
+let accountEmail="abcd@google.com" 
+var accountPassword="12345" //not used anymore
+accountCity="Kolkata" //not a good method
+
+//console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])  //every variable in tabular form, better than printing all the variables separately.
+
+/*variable declared but no value= undefined */
