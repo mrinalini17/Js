66dadc1649e7e98c06426cc0e7cd2017eb78ab2b
Mrinalini Bhattacharjee <138326214+mrinalini17@users.noreply.github.com>
2024-12-13 19:23:51 +0000

    Conversions pt2

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -5,3 +5,10 @@ console.log(typeof(score))
 let valueInNumber=Number(score);
 console.log(typeof(valueInNumber))
 console.log(valueInNumber)
+
+//*********Operations **********
+
+console.log(2+2)
+
+//("1"+2+2)=122
+//(1+2+"2")=32
