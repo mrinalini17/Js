b4c31562af02313fc561df907ec52a1c9ae31443
Mrinalini Bhattacharjee <138326214+mrinalini17@users.noreply.github.com>
2024-12-13 19:28:11 +0000

    Comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,13 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+//console.log(2!=1);
+
+//change datatype if the datatypes are different.
+
+//null>=0: true
+
+//=== checks datatypes also
+
+//== not strict, checks the value
