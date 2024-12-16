e23c57c9cb815eb3e69243bb8cbf8eaa252913e2
Mrinalini Bhattacharjee <138326214+mrinalini17@users.noreply.github.com>
2024-12-16 19:36:13 +0000

    Array part 1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,11 @@
+//array
+
+const myArr=[0,1,2,3,4,5,true,"Mrinalini"]
+const myArr2=new Array(1,2,3,4)
+console.log(myArr[0]);
+console.log(myArr2[0]);
+//same reference point
+
+myArr.push(6)
+//pop: last element
+//unshift(9): inserted at starting
