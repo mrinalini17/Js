8e3bb17051c5450de0811d4366e37567de0e450a
Mrinalini Bhattacharjee <138326214+mrinalini17@users.noreply.github.com>
2024-12-16 19:16:47 +0000

    Numbers

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,30 @@
+/*const score=400
+console.log(score);
+
+const balance= new Number(200)
+console.log(balance);
+
+console.log(balance.toString().length);
+
+const othernumber=13423.889
+console.log(othernumber.toPrecision(3));
+console.log(othernumber.toLocaleString());*/
+
+
+// ******************MATHs********************
+
+
+/*console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));
+console.log(Math.sqrt(25));
+console.log(Math.max(4,6,2,3));*/
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
