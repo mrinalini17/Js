6083c72158b380ce5d594aa282731bc216ec2e56
Mrinalini Bhattacharjee <138326214+mrinalini17@users.noreply.github.com>
2024-12-16 19:16:35 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,13 @@
+const name="Mrinalini"
+const repocount=50
+
+//console.log(name+repocount+"value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.charAt(2));
+console.log(name.indexOf('i'));
+
+const name1="   mrinalini   "
+console.log(name1.trim());
