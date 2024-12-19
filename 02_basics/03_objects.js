3e5920e9d05671977a7875082f1d9268a50f15c1
Mrinalini Bhattacharjee <138326214+mrinalini17@users.noreply.github.com>
2024-12-19 18:10:14 +0000

    Objects pt 1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,16 @@
+//singleton
+
+//object literals
+
+const JsUser={
+    name: "Mrinalini",
+    age: 21,
+    location: "Bengalurur"
+}
+
+console.log(JsUser.name)
+console.log(JsUser["name"])//better because full name can't be used with .
+
+//values can be overwritten with =
+//values can be freezed using Object.freeze(JsUser)
+
